5584cbc77c9e86f669204a2522b0cb59ecf95d8c
Andres Vazquez <andres@data99.com.ar>
2022-07-27 15:46:40 -0300

    Fix download script

--- download-static-site.sh
+++ download-static-site.sh
@@ -82,4 +82,4 @@ echo "Delete local folder"
 mv $DOMAIN_NAME "${DOMAIN_NAME}-DONE"
 
 echo "Set up CNAME record"
-python update_cname_records.py $BUCKET_NAME false
+python update_cname_records.py $BUCKET_NAME c.storage.googleapis.com false
