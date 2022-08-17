23d6808b90ee28637bd01d52f32d8653e3c55e4c
Andres Vazquez <andres@data99.com.ar>
2022-08-17 15:45:49 -0300

    Fix, show all CNAME records

--- get_all_dns_records.py
+++ get_all_dns_records.py
@@ -6,7 +6,7 @@ zones = cf.zones.get(params={'per_page':150})
 
 for zone in zones:
     records = cf.zones.dns_records.get(
-        zone['id'], params={"name": zone['name'], "type": "CNAME"}
+        zone['id'], params={"type": "CNAME"}
     )
     for record in records:
         print(zone['name'], record['type'], record['content'])
